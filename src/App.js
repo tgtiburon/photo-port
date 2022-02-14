b715ae4a96f168de90e8eec598d3cba04fc565c6
tgtiburon <tg.tiburon@gmail.com>
2022-02-14 14:18:36 -0500

    about finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
+  // If we wrote it without JSX
+  // Returns JSX   -> Javascript XML
+  // React.createElement("div", {} [
+  // React.createElement("h1", {}, "Ginger"),
+  // React.createElement("p", {}, "Breed: Brittany Spaniel")
+ // ])
+ // Same as 
+ // <div>
+    // <h1>Ginger</h1>
+    // <p>bree: Brittany Spaniel</p>
+ //</div>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
