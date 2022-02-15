0ae7a0054366b2fbcaa5f6bd3f8cdf9505a7907b
tgtiburon <tg.tiburon@gmail.com>
2022-02-15 18:53:55 -0500

    before class

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,35 +1,73 @@
-import react from "react";
+import react, { useState } from "react";
+
+// initialize the values of the state
 
 function ContactForm() {
-    //JSX
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
+  //JSX
+  // destructure formState
+  const { name, email, message } = formState;
 
-    // Have to use htmlFor because it is a keyword reserved for javascript.
-    // same with for  
-    return (
-        <section>
-            <h1>Contact me</h1>
-            <form id="contact-form">
-               
-                <div> 
-                    <label htmlFor="name">Name:</label> 
-                    <input type='text' name="name" />
-                </div>
-              
-                <div>
-                    <label htmlFor="email">Email address:</label> 
-                    <input type='email' name="email" />
-                </div>
-             
-                <div>
-                    <label htmlFor="message">Message:</label> 
-                    <input type='message' rows="5" />
-                </div>
-                <button type="submit">Submit</button>
-            </form>
-        </section>
-    )
+  // Change in the html
 
+  function handleChange(e) {
+    // without the spread operator ..., the formState object would be
+    // overwritten to only contain the name:value pair
+    // [e.target.name]  lets us DRY, so we can reuse setFormState
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+  // placed outside the handleChange function because setFormState is asynchronous.
+  //console.log(formState);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  // Have to use htmlFor because it is a keyword reserved for javascript.
+  // same with for
+  return (
+    <section>
+      <h1>Contact me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            defaultValue={name}
+            onChange={handleChange}
+            name="name"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            defaultValue={email}
+            onChange={handleChange}
+            name="email"
+          />
+        </div>
+
+        <div>
+          <textarea htmlFor="message">Message:</textarea>
+          <input
+            type="message"
+            defaultValue={message}
+            onChange={handleChange}
+            rows="5"
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
 }
 
 export default ContactForm;
