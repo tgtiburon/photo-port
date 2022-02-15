9d7223645f9b847b9ade1ede9cd5540ee76b1a28
tgtiburon <tg.tiburon@gmail.com>
2022-02-15 13:35:03 -0500

    finished useEffect for states

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-
-import './App.css';
-import About from './components/About';
-import Nav from './components/Nav';
+import React, { useState } from "react";
+import About from "./components/About";
+import Nav from "./components/Nav";
+import Gallery from "./components/Gallery";
 
 function App() {
+  const [categories] = useState([
+    {
+      name: "commerical",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
   // If we wrote it without JSX
   // Returns JSX   -> Javascript XML
   // React.createElement("div", {} [
   // React.createElement("h1", {}, "Ginger"),
   // React.createElement("p", {}, "Breed: Brittany Spaniel")
- // ])
- // Same as 
- // <div>
-    // <h1>Ginger</h1>
-    // <p>bree: Brittany Spaniel</p>
- //</div>
+  // ])
+  // Same as
+  // <div>
+  // <h1>Ginger</h1>
+  // <p>bree: Brittany Spaniel</p>
+  //</div>
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
